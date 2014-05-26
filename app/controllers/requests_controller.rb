f7fb8293cab98b83b656bf375f0084259765509d
portOdin <dominic.n@me.com>
2014-05-25 23:13:44 -0400

    blah

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -4,12 +4,12 @@ class RequestsController < ApplicationController
   # GET /requests
   # GET /requests.json
   def index
-	  @requests = Offer.all
+	  @requests = Request.all
 
 	  if params[:search]
-		  @requests = Offer.search(params[:search]).order("created_at DESC")
+		  @requests = Request.search(params[:search]).order("created_at DESC")
 	  else
-		  @requests = Offer.all.order('created_at DESC')
+		  @requests = Request.all.order('created_at DESC')
 	  end
   end
 
