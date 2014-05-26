296255a849a3202a101042f05d0b8eb227b98849
portOdin <dominic.n@me.com>
2014-05-26 00:34:54 -0400

    added text to navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.js
+//= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
+//= require foundation
 //= require_tree .
-
+$(function() {
+  $(document).foundation();
+});
